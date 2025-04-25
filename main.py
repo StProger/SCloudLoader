ac84255d684f6afe0961462c1fbe57bf80549c8f
Kirill Mikhailov <kicode@mail.ru>
2025-04-25 17:34:07 +0300

    fix pyrogram

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def main():
     if not(os.path.exists("bot/service/sound_cloud/tracks")):
         os.mkdir("bot/service/sound_cloud/tracks")
     storage = RedisStorage.from_url(settings.fsm_redis_url)
-    await client.start()
+    await client.connect()
     dp = Dispatcher(storage=storage)
     dp["client"] = client
 
@@ -49,7 +49,7 @@ async def main():
         sys.exit(1)
     finally:
         await bot.session.close()
-        await client.stop()
+        await client.disconnect()
 
 
 if __name__ == '__main__':
