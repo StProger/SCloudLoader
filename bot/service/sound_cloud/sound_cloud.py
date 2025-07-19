fb9baf61be9ba05b39010710eacabb4f079e3cfc
Kirill Mihaylov <kirill.mihaylov@ifellow.ru>
2025-07-19 19:21:27 +0300

    hotfix

--- bot/service/sound_cloud/sound_cloud.py
+++ bot/service/sound_cloud/sound_cloud.py
@@ -51,7 +51,8 @@ class SoundCloud():
 
             file_path = f'bot/service/sound_cloud/tracks/{user_id}'
             file_path_archive = f"bot/service/sound_cloud/tracks/archive/{user_id}"
-
+            if not os.path.exists(file_path_archive):
+                os.mkdir(file_path_archive)
             cls.proces_download_track(file_path=file_path, url=track_url)
             list_files = os.listdir(file_path)
             print(list_files)
