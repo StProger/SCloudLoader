8e6a7eebb91827ab77f86da0ad0665452558cfbd
Kirill Mihaylov <kirill.mihaylov@ifellow.ru>
2025-07-19 18:48:52 +0300

    fix celery tasks

--- bot/celery_app.py
+++ bot/celery_app.py
@@ -3,7 +3,8 @@ from celery import Celery
 celery_app = Celery(
     "telegram_tasks",
     broker="redis://redis:6379/0",
-    backend="redis://redis:6379/1"
+    backend="redis://redis:6379/1",
+    include=["bot.tasks"]
 )
 
 celery_app.conf.timezone = 'UTC'
