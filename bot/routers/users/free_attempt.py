fc913c37c5d221850ed4b3d3b4afa0f704bbbfdb
Kirill Mihaylov <kirill.mihaylov@ifellow.ru>
2025-07-23 18:38:29 +0300

    Пофиксил логику бесплатных попыток

--- bot/routers/users/free_attempt.py
+++ bot/routers/users/free_attempt.py
@@ -73,7 +73,7 @@ async def download_music(
             message.text,
             downloaded_msg.message_id
         )
-        user.free_attempts = user.free_attempts - 1
+        user.free_attempts = user.free_attempts + 1
         await user.save()
 
     await state.clear()
